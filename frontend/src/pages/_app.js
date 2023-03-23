9c5b8170a211bc77b91311c4aac639f2961564a5
Kent Ntwari <designerjehovah@gmail.com>
2023-03-23 10:24:50 -0400

    initial scaffolding of tailwind classes in pages

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import '../styles/globals.css';
+import { Poppins } from 'next/font/google';
+
+import Layout from '@/components/Layout';
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700', '800'],
+  style: ['normal', 'italic'],
+  variable: '--font-poppins',
+  preload: false,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={`min-h-screen flex flex-col ${poppins.variable} font-sans font-normal`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-lp-300 dark:bg-dp-300 text-ls-300 dark:text-white-300 text-sm lg:text-base">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
