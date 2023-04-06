fad27f489c3e8dba6f34ef99f23fca2578d23e2c
Kent Ntwari <designerjehovah@gmail.com>
2023-04-06 12:09:15 -0400

    updated file

--- strapi-backend/config/database.js
+++ strapi-backend/config/database.js
@@ -2,14 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "db.vgkkltsttxaezurkgcyn.supabase.co"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "Nakusifu96!"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA"), // Not required
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF"),
       },
     },
     debug: false,
