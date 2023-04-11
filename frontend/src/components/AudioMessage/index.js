e50601ec6c53ff4cdbf97eb78cda65e1126fec8b
Kent Ntwari <designerjehovah@gmail.com>
2023-04-11 13:49:04 -0400

    export all substates of an audio message component

---
+++ frontend/src/components/AudioMessage/index.js
@@ -0,0 +1,10 @@
+import Snippet from './Snippet';
+
+const AudioMessage = {
+  // exports a Snippet of the message with just...
+  // a play button and a duration of the audio...
+  //...as well as a link to the full audio message post
+  Snippet,
+};
+
+export default AudioMessage;
