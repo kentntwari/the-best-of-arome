183bc8b2d4fe418086e1ba563cbc539210e4c53f
Kent Ntwari <designerjehovah@gmail.com>
2023-05-02 19:20:52 -0400

    updated method

--- frontend/src/components/AudioPlayer/utils/convertToMinutesSeconds.js
+++ frontend/src/components/AudioPlayer/utils/convertToMinutesSeconds.js
@@ -1,8 +1,9 @@
 export function convertToMinutesSeconds(duration) {
+  if (isNaN(duration) || !duration) return null;
+
   let minutes = Math.floor(duration / 60);
   minutes = minutes < 10 ? '0' + minutes : minutes;
   let seconds = Math.floor(duration % 60);
   seconds = seconds < 10 ? '0' + seconds : seconds;
-
   return `${minutes}:${seconds}`;
 }
