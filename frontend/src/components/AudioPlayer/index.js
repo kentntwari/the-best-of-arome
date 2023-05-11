6f1ffc333861f327ba6b5d82f4e272018db2b5ee
Kent Ntwari <designerjehovah@gmail.com>
2023-05-10 21:46:21 -0400

    updated component
    encapsulated essential functionality in one hook

--- frontend/src/components/AudioPlayer/index.js
+++ frontend/src/components/AudioPlayer/index.js
@@ -1,53 +1,39 @@
-import { memo, useRef } from 'react';
-
+import { useAudioPlayerContext } from '@/hooks/useAudioPlayerContext';
 import { useAudioPlayer } from '@/hooks/useAudioPlayer';
 
-import {
-  PlayCircleIcon,
-  PauseCircleIcon,
-  ForwardIcon,
-  BackwardIcon,
-} from '@heroicons/react/24/solid';
+import { Controls } from './Controls';
 
-const Back = memo(BackwardIcon);
-const Play = memo(PlayCircleIcon);
-const Pause = memo(PauseCircleIcon);
-const Forward = memo(ForwardIcon);
+const AudioPlayer = ({ type = null }) => {
+  const url = useAudioPlayerContext();
 
-const AudioPlayer = ({ type = null, playing: url }) => {
-  // reference DOM elements and values in memory
-  const audioPlayer_ref = useRef();
+  // import all functionality from one hook
+  const { exportedComponents, state, methods } = useAudioPlayer(url);
+
+  // components object varibale to allow dot notation
+  const Exported = exportedComponents;
 
   const {
-    exportedComponents: { CurrentTime, Duration, ProgressBar },
-    state,
-    methods,
-  } = useAudioPlayer(audioPlayer_ref.current);
+    playAudio,
+    pauseAudio,
+    updateCurrentTime: { forwardAudio, backwardAudio },
+  } = methods;
 
   // render component when only play button and total duration is needed
   if (type === 'playonly')
     return (
       <div className="flex items-center gap-2">
-        <audio
-          ref={audioPlayer_ref}
-          src={url}
-          preload="metadata"
-          onLoadedMetadata={() =>
-            methods.updateDuration(audioPlayer_ref.current.duration)
-          }
-          onPlay={methods.updateAudioState.isPlaying}
-          onPause={methods.updateAudioState.isPaused}
-        />
-
-        <Duration />
+        <Exported.Duration />
 
         {state.audioIsPlaying ? (
-          <Pause
-            onClick={methods.pauseAudio}
+          <Controls.Pause
+            onClick={pauseAudio}
             className="w-10 text-ls-300 cursor-pointer"
           />
         ) : (
-          <Play onClick={methods.playAudio} className="w-10 text-ls-400 cursor-pointer" />
+          <Controls.Play
+            onClick={playAudio}
+            className="w-10 text-ls-400 cursor-pointer"
+          />
         )}
       </div>
     );
@@ -55,34 +41,28 @@ const AudioPlayer = ({ type = null, playing: url }) => {
   // full fledged-audio player component
   return (
     <div className="bg-la-100 p-5 flex flex-col gap-5">
-      <audio
-        ref={audioPlayer_ref}
-        src={url}
-        preload="metadata"
-        onLoadedMetadata={() => methods.updateDuration(audioPlayer_ref.current.duration)}
-        onPlay={methods.updateAudioState.isPlaying}
-        onPause={methods.updateAudioState.isPaused}
-      />
-
       <div className="grid grid-cols-[35px_1fr_35px] items-center gap-1">
-        <CurrentTime />
-        <ProgressBar />
-        <Duration />
+        <Exported.CurrentTime />
+        <Exported.ProgressBar />
+        <Exported.Duration />
       </div>
 
       <div className="flex items-center justify-center gap-4">
-        <Back className="w-7.5 text-ls-300" />
+        <Controls.Back onClick={backwardAudio} className="w-7.5 text-ls-300" />
 
         {state.audioIsPlaying ? (
-          <Pause
-            onClick={methods.pauseAudio}
+          <Controls.Pause
+            onClick={pauseAudio}
             className="w-10 text-ls-300 cursor-pointer"
           />
         ) : (
-          <Play onClick={methods.playAudio} className="w-10 text-ls-400 cursor-pointer" />
+          <Controls.Play
+            onClick={playAudio}
+            className="w-10 text-ls-400 cursor-pointer"
+          />
         )}
 
-        <Forward className="w-7.5 text-ls-300" />
+        <Controls.Forward onClick={forwardAudio} className="w-7.5 text-ls-300" />
       </div>
     </div>
   );
