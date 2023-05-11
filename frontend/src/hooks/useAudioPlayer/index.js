0bae1819afef50dd30c26402db99f839289454d8
Kent Ntwari <designerjehovah@gmail.com>
2023-05-10 21:49:44 -0400

    updated hook
    all functionality encapsulated

--- frontend/src/hooks/useAudioPlayer/index.js
+++ frontend/src/hooks/useAudioPlayer/index.js
@@ -2,22 +2,17 @@ import { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 
 import useEventListener from '@use-it/event-listener';
 
-import { convertToMinutesSeconds as formatTime } from '@/hooks/useAudioPlayer/utils/convertToMinutesSeconds';
+import { EVENTS } from './utils/audioEvents';
+import { convertToMinutesSeconds as formatTime } from './utils/convertToMinutesSeconds';
 
-const useAudioPlayer = (player) => {
+const useAudioPlayer = (url) => {
   // Barebones state that need to be rendered or re-rendered
+  const [isReadyToPlay, setIsReadyToPlay] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(null);
 
-  // render duration on mount
-  useEffect(() => {
-    setDuration(player?.duration);
-  }, [player?.duration]);
-
-  // render initial value of current audio time
-  useEffect(() => {
-    if (currentTime_ref.current) currentTime_ref.current.innerText = '00:00';
-  }, []);
+  // instantiate reference for dynamic audio element
+  const audio_ref = useRef();
 
   // reference the duration as it doesn't change...
   // ...per audio file that is playing
@@ -25,30 +20,135 @@ const useAudioPlayer = (player) => {
   duration_ref.current = duration;
 
   // reference the current time from a HTML element...
-  // ...as th eupdated value will be directly inserted...
+  // ...as the updated value will be directly inserted...
   // ...in the DOM
   const currentTime_ref = useRef();
 
   // reference the progress bar html element
   const progressBar_ref = useRef();
 
+  useEffect(() => {
+    if (!url) {
+      setIsReadyToPlay(false);
+      console.error('No url was given as an argument for useAudioPlayer hook');
+    }
+
+    setIsReadyToPlay(true);
+
+    audio_ref.current = new Audio();
+    audio_ref.current.preload = 'metadata';
+    audio_ref.current.src = url;
+  }, [url]);
+
+  // render initial value of current audio time
+  useEffect(() => {
+    if (currentTime_ref.current) currentTime_ref.current.innerText = '00:00';
+  }, []);
+
+  const displayAudioCurrentTime = useCallback(() => {
+    if (currentTime_ref.current)
+      currentTime_ref.current.innerText =
+        formatTime(audio_ref.current?.currentTime) ?? '00:00';
+  }, []);
+
+  const fillProgressBarBackground = useCallback(() => {
+    progressBar_ref.current.value =
+      (audio_ref.current?.currentTime / duration_ref.current) * 100;
+    progressBar_ref.current.style.background = `linear-gradient(to right,#ae7137 0 ${progressBar_ref.current.value}%,#dedede 0)`;
+  }, []);
+
   // calculate expended audio time with animation frame...
   // ...to avoid needless re-rendering
   let animate = false;
 
-  let update = () => {
-    if (currentTime_ref.current)
-      currentTime_ref.current.innerText = formatTime(player?.currentTime);
+  let update = useCallback(() => {
+    displayAudioCurrentTime();
 
     if (progressBar_ref.current) {
-      progressBar_ref.current.value = (player?.currentTime / duration_ref.current) * 100;
-      progressBar_ref.current.style.background = `linear-gradient(to right,#ae7137 0 ${progressBar_ref.current.value}%,#dedede 0)`;
+      fillProgressBarBackground();
     }
 
     if (animate === true) {
       requestAnimationFrame(update);
     }
-  };
+  }, []);
+
+  // set audio state to isPlaying...
+  const readAudioIsPlaying = useCallback(() => {
+    return setIsPlaying(true);
+  }, []);
+
+  // set audio state to pause...
+  const readAudioIsPaused = useCallback(() => {
+    return setIsPlaying(false);
+  }, []);
+
+  // save play audio function
+  const playAudio = useCallback(() => {
+    animate = true;
+    window.requestAnimationFrame(update);
+
+    return audio_ref.current?.play();
+  }, []);
+
+  // save pause audio function
+  const pauseAudio = useCallback(() => {
+    animate = false;
+    window.cancelAnimationFrame(update);
+
+    return audio_ref.current?.pause();
+  }, []);
+
+  const seekTimeframe = useCallback((e) => {
+    audio_ref.current.currentTime = (duration_ref.current * e.target.value) / 100;
+    displayAudioCurrentTime();
+    fillProgressBarBackground();
+  }, []);
+
+  const forwardAudio = useCallback(() => {
+    if (audio_ref.current?.currentTime < duration_ref.current) {
+      audio_ref.current.currentTime += 0.5;
+      displayAudioCurrentTime();
+      fillProgressBarBackground();
+    }
+  }, [audio_ref.current?.currentTime]);
+
+  const backwardAudio = useCallback(() => {
+    if (audio_ref.current?.currentTime >= 0) {
+      audio_ref.current.currentTime -= 0.5;
+      displayAudioCurrentTime();
+      fillProgressBarBackground();
+    }
+  }, [audio_ref.current?.currentTime]);
+
+  // audio event listener to calculate how many...
+  // ...frames are available to be played
+  useEventListener(
+    EVENTS.LOADED,
+    () => {
+      if (audio_ref.current.readyState === 0) {
+        setIsReadyToPlay(false);
+        console.warn('no enough frame loaded to play file');
+      }
+
+      setIsReadyToPlay(true);
+    },
+    audio_ref.current
+  );
+
+  // audio event listener that sends duration...
+  // ..as soon as enough metdata is available
+  useEventListener(
+    EVENTS.LOADED_METADATA,
+    () => setDuration(audio_ref.current?.duration),
+    audio_ref.current
+  );
+
+  // audio event listener that listens if the audio is playing
+  useEventListener(EVENTS.PLAY_AUDIO, readAudioIsPlaying, audio_ref.current);
+
+  // audio event listener that listens if audio is paused
+  useEventListener(EVENTS.PAUSE_AUDIO, readAudioIsPaused, audio_ref.current);
 
   // Exported JSX element for current time
   const CurrentTime = useMemo(
@@ -80,6 +180,7 @@ const useAudioPlayer = (player) => {
       (
         <input
           ref={progressBar_ref}
+          onChange={seekTimeframe}
           type="range"
           step={0.5}
           defaultValue={0}
@@ -91,32 +192,6 @@ const useAudioPlayer = (player) => {
     []
   );
 
-  // set audio state to isPlaying...
-  const readAudioIsPlaying = useCallback(() => {
-    return setIsPlaying(true);
-  }, []);
-
-  // set audio state to pause...
-  const readAudioIsPaused = useCallback(() => {
-    return setIsPlaying(false);
-  }, []);
-
-  // save play audio function
-  const playAudio = () => {
-    animate = true;
-    window.requestAnimationFrame(update);
-
-    return player?.play();
-  };
-
-  // save pause audio function
-  const pauseAudio = () => {
-    animate = false;
-    window.cancelAnimationFrame(update);
-
-    return player?.pause();
-  };
-
   return {
     exportedComponents: {
       CurrentTime,
@@ -124,6 +199,7 @@ const useAudioPlayer = (player) => {
       ProgressBar,
     },
     state: {
+      ready: isReadyToPlay,
       audioIsPlaying: isPlaying,
     },
     methods: {
@@ -133,6 +209,10 @@ const useAudioPlayer = (player) => {
       updateAudioState: {
         isPlaying: readAudioIsPlaying,
         isPaused: readAudioIsPaused,
+      },
+      updateCurrentTime: {
+        forwardAudio,
+        backwardAudio,
       },
       updateDuration: (newDuration) => setDuration(newDuration),
     },
---
+++ frontend/src/hooks/useAudioPlayer/utils/audioEvents.js
@@ -0,0 +1,6 @@
+export const EVENTS = {
+  LOADED_METADATA: 'loadedmetadata',
+  LOADED: 'loaded',
+  PLAY_AUDIO: 'play',
+  PAUSE_AUDIO: 'pause',
+};
