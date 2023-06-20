ff462d838881f4db85a661c5259e1e59fa4baf1a
Kent Ntwari <designerjehovah@gmail.com>
2023-06-20 08:42:33 -0400

    update: centralized all components in useAudioPlayer hook

--- frontend/src/components/AudioPlayer/Provider.js
+++
@@ -1,9 +0,0 @@
-import { AudioPlayerContext } from '@/context/audioPlayer';
-
-const AudioPlayerProvider = ({ value, children }) => {
-  return (
-    <AudioPlayerContext.Provider value={value}>{children}</AudioPlayerContext.Provider>
-  );
-};
-
-export default AudioPlayerProvider;
--- frontend/src/components/AudioPlayer/index.js
+++ frontend/src/components/AudioPlayer/index.js
@@ -1,70 +1,24 @@
-import { useAudioPlayerContext } from '@/hooks/useAudioPlayerContext';
 import { useAudioPlayer } from '@/hooks/useAudioPlayer';
 
-import { Controls } from './Controls';
+const AudioPlayer = (props) => {
+  const { Components } = useAudioPlayer(props.title, props.slug, props.url);
 
-const AudioPlayer = ({ type = null }) => {
-  const url = useAudioPlayerContext();
-
-  // import all functionality from one hook
-  const { exportedComponents, state, methods } = useAudioPlayer(url);
-
-  // components object varibale to allow dot notation
-  const Exported = exportedComponents;
-
-  const {
-    playAudio,
-    pauseAudio,
-    updateCurrentTime: { forwardAudio, backwardAudio },
-  } = methods;
-
-  // render component when only play button and total duration is needed
-  if (type === 'playonly')
-    return (
-      <div className="flex items-center gap-2">
-        <Exported.Duration />
-
-        {state.audioIsPlaying ? (
-          <Controls.Pause
-            onClick={pauseAudio}
-            className="w-10 text-ls-300 cursor-pointer"
-          />
-        ) : (
-          <Controls.Play
-            onClick={playAudio}
-            className="w-10 text-ls-400 cursor-pointer"
-          />
-        )}
-      </div>
-    );
-
-  // full fledged-audio player component
   return (
-    <div className="bg-la-100 p-5 flex flex-col gap-5">
-      <div className="grid grid-cols-[35px_1fr_35px] items-center gap-1">
-        <Exported.CurrentTime />
-        <Exported.ProgressBar />
-        <Exported.Duration />
+    <>
+      <div className="bg-la-100 p-5 flex flex-col gap-5">
+        <div className="grid grid-cols-[35px_1fr_35px] items-center gap-1">
+          <Components.CurrentTime />
+          <Components.ProgressBar />
+          <Components.Duration />
+        </div>
+
+        <div className="flex items-center justify-center gap-4">
+          <Components.Back />
+          <Components.Play />
+          <Components.Forward />
+        </div>
       </div>
-
-      <div className="flex items-center justify-center gap-4">
-        <Controls.Back onClick={backwardAudio} className="w-7.5 text-ls-300" />
-
-        {state.audioIsPlaying ? (
-          <Controls.Pause
-            onClick={pauseAudio}
-            className="w-10 text-ls-300 cursor-pointer"
-          />
-        ) : (
-          <Controls.Play
-            onClick={playAudio}
-            className="w-10 text-ls-400 cursor-pointer"
-          />
-        )}
-
-        <Controls.Forward onClick={forwardAudio} className="w-7.5 text-ls-300" />
-      </div>
-    </div>
+    </>
   );
 };
 
