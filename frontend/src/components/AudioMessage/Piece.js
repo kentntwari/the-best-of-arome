aea56945e57f561628ffcf4fb867e559ab42b8e0
Kent Ntwari <designerjehovah@gmail.com>
2023-06-24 15:19:32 -0400

    feat: added duration to pill buttons in playlist

--- frontend/src/components/AudioMessage/Piece.js
+++ frontend/src/components/AudioMessage/Piece.js
@@ -1,16 +1,25 @@
 import { useState, useCallback, useRef } from 'react';
 
-import { useAudioPlayer } from '@/hooks/useAudioPlayer';
-import { convertToMinutesSeconds } from '@/hooks/useAudioPlayer/utils/convertToMinutesSeconds';
+import { convertToMinutesSeconds as formatTime } from '@/hooks/useAudioPlayer/utils/convertToMinutesSeconds';
 
 import AudioModal from '../AudioModal';
 
 const Piece = ({ data, trackBg }) => {
   const { title, url } = data;
 
+  // make details dynamic since we expect diverse results
   const [modalDetails, setModalDetails] = useState({});
 
+  // reference modal in the system
   const modal_ref = useRef();
+  // reference audio for each piece
+  const audio_ref = useRef();
+  // reference unique duration for each audio source...
+  const duration_ref = useRef();
+
+  function loadAudioDuration() {
+    duration_ref.current.innerText = formatTime(audio_ref.current?.duration);
+  }
 
   const displayModalDetails = useCallback(() => {
     setModalDetails((prev) => {
@@ -22,6 +31,12 @@ const Piece = ({ data, trackBg }) => {
 
   return (
     <>
+      <audio
+        ref={audio_ref}
+        src={url}
+        preload="metadata"
+        onLoadedMetadata={loadAudioDuration}
+      />
       <AudioModal ref={modal_ref} {...modalDetails} />
 
       <div
@@ -29,7 +44,9 @@ const Piece = ({ data, trackBg }) => {
         className={`px-2 py-4 ${trackBg} flex items-center`}>
         <p className="grow font-semibold text-xs text-ls-400">{title}</p>
 
-        <span className='w-16 h-6 rounded bg-ls-300 flex items-center justify-center text-white-300 text-xs"'>
+        <span
+          ref={duration_ref}
+          className='w-16 h-6 rounded bg-ls-300 flex items-center justify-center text-white-300 text-xs"'>
           {'--:--'}
         </span>
       </div>
