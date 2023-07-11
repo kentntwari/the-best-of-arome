a2c27163264b93b4c837f9a217d01460330fd25e
Kent Ntwari <designerjehovah@gmail.com>
2023-07-11 10:37:13 -0400

    feat:refactored component

--- frontend/src/components/AudioMessage/Piece.js
+++ frontend/src/components/AudioMessage/Piece.js
@@ -1,17 +1,8 @@
-import { useState, useCallback, useRef } from 'react';
+import { useRef } from 'react';
 
 import { convertToMinutesSeconds as formatTime } from '@/hooks/useAudioPlayer/utils/convertToMinutesSeconds';
 
-import AudioModal from '../AudioModal';
-
 const Piece = ({ data, trackBg }) => {
-  const { title, url } = data;
-
-  // make details dynamic since we expect diverse results
-  const [modalDetails, setModalDetails] = useState({});
-
-  // reference modal in the system
-  const modal_ref = useRef();
   // reference audio for each piece
   const audio_ref = useRef();
   // reference unique duration for each audio source...
@@ -21,28 +12,17 @@ const Piece = ({ data, trackBg }) => {
     duration_ref.current.innerText = formatTime(audio_ref.current?.duration);
   }
 
-  const displayModalDetails = useCallback(() => {
-    setModalDetails((prev) => {
-      return { ...prev, title, url };
-    });
-
-    modal_ref.current.showModal();
-  }, []);
-
   return (
     <>
       <audio
         ref={audio_ref}
-        src={url}
+        src={data.url}
         preload="metadata"
         onLoadedMetadata={loadAudioDuration}
       />
-      <AudioModal ref={modal_ref} {...modalDetails} />
 
-      <div
-        onClick={displayModalDetails}
-        className={`px-2 py-4 ${trackBg} flex items-center`}>
-        <p className="grow font-semibold text-xs text-ls-400">{title}</p>
+      <div className={`px-2 py-4 ${trackBg} flex items-center`}>
+        <p className="grow font-semibold text-xs text-ls-400">{data.title}</p>
 
         <span
           ref={duration_ref}
