fa709017f916c0b4a42d9e56cacc210dff7765e2
Kent Ntwari <designerjehovah@gmail.com>
2023-07-29 11:30:52 -0400

    new  structure
    values nested into objects for better readability

--- frontend/src/context/PlayerContext.js
+++ frontend/src/context/PlayerContext.js
@@ -6,7 +6,8 @@ const PlayerContext = createContext(null);
 
 const PlayerProvider = ({ children }) => {
   const [url, setUrl] = useState(null);
-  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [globalAudioDuration, setGlobalAudioDuration] = useState(null);
+  const [isGlobalAudioPlaying, setIsGlobalAudioPlaying] = useState(false);
 
   const audio_ref = useRef();
   const [playerDetails] = useSWRAudioState();
@@ -23,11 +24,22 @@ const PlayerProvider = ({ children }) => {
         ref={audio_ref}
         src={url}
         preload="metadata"
-        onPlay={() => setIsAudioPlaying(true)}
-        onPause={() => setIsAudioPlaying(false)}
+        onLoadedMetadata={() => setGlobalAudioDuration(audio_ref.current.duration)}
+        onPlay={() => setIsGlobalAudioPlaying(true)}
+        onPause={() => setIsGlobalAudioPlaying(false)}
       />
       <PlayerContext.Provider
-        value={{ ref: audio_ref, isAudioPlaying, forceAudioPlay: setIsAudioPlaying }}>
+        value={{
+          ref: audio_ref.current,
+          globalAudioState: {
+            isPlaying: isGlobalAudioPlaying,
+            duration: globalAudioDuration,
+          },
+          forceGlobalAudioState: {
+            toPlay: () => setIsGlobalAudioPlaying(true),
+            toPause: () => setIsGlobalAudioPlaying(false),
+          },
+        }}>
         {children}
       </PlayerContext.Provider>
     </>
