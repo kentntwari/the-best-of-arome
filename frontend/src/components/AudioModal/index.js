ad2ba381d1ad1fd69664a765061ebeb4d735c69c
Kent Ntwari <designerjehovah@gmail.com>
2023-07-29 14:49:12 -0400

    updated component logic

--- frontend/src/components/AudioModal/index.js
+++ frontend/src/components/AudioModal/index.js
@@ -1,42 +1,39 @@
 import { forwardRef } from 'react';
 
-import { XCircleIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid';
-
 import { useSWRAudioState } from '@/hooks/useSWRAudioState';
-import { usePlayerContext } from '@/hooks/usePlayerContext';
 import { useAudioPlayer } from '@/hooks/useAudioPlayer';
 
+import { XCircleIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid';
+
+import AudioPlayer from '../AudioPlayer';
+import AudioWavesAnimation from './AudioWavesAnimation';
+
 const AudioModal = forwardRef((props, ref) => {
-  const context = usePlayerContext();
-  const { Components } = useAudioPlayer();
+  const { methods } = useAudioPlayer();
   const [playerDetails] = useSWRAudioState();
 
+  function closeModal() {
+    methods.pauseAudio();
+    ref.current.close();
+    document.body.removeAttribute('style');
+  }
+
   return (
-    <dialog ref={ref} className="w-[335px] h-[400px] p-0 border-none appearance-none">
-      <XCircleIcon className="w-6 absolute top-1 right-1 text-neutral-10" />
-      <div className="bg-black-300 w-full h-32"></div>
+    <dialog ref={ref} className="w-[335px] p-0 border-none rounded-lg appearance-none">
+      <XCircleIcon
+        className="w-6 absolute top-1 right-1 text-neutral-10 cursor-pointer"
+        onClick={closeModal}
+      />
+
+      <AudioWavesAnimation />
+
       <section className="p-3 flex flex-col gap-10">
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-4">
           <span className="font-semibold text-base text-ls-500">
             {playerDetails.title}
           </span>
-          <div className="flex items-center gap-2">
-            <Components.CurrentTime variant="text-ls-500" />
-            <Components.ProgressBar />
-            <Components.Duration variant="text-ls-500" />
-          </div>
 
-          <div className="flex justify-center items-center gap-4">
-            <Components.Back variant="text-ls-500" />
-
-            {context.isAudioPlaying === false ? (
-              <Components.Play variant="text-ls-500" />
-            ) : (
-              <Components.Pause variant="text-ls-500" />
-            )}
-
-            <Components.Forward variant="text-ls-500" />
-          </div>
+          <AudioPlayer />
         </div>
 
         <a href="/" className="flex self-center">
