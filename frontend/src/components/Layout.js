c666535676e6f14c7abf640f35409978471a872d
Kent Ntwari <designerjehovah@gmail.com>
2023-08-04 09:20:39 -0400

    updated componentt
    display AudioBanner on specified routes

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,12 +1,21 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
+import { useRouter } from "next/router";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import AudioBanner from "./AudioBanner";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
 
   return (
     <>
       <Navbar />
+
+      {router.pathname === "/" && <AudioBanner />}
+      {router.pathname === "/browse" && !router.query.playlist && <AudioBanner />}
+
       <div className="grow">{children}</div>
+
       <Footer />
     </>
   );
