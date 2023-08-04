283bd71839fbbb8b7ea4084286927924d3f22caf
Kent Ntwari <designerjehovah@gmail.com>
2023-08-04 09:19:03 -0400

    updated hook

--- frontend/src/hooks/useAudioPlayer.js
+++ frontend/src/hooks/useAudioPlayer.js
@@ -1,47 +1,43 @@
-import { useCallback } from 'react';
-
-import { usePlayerContext } from './usePlayerContext';
+import { usePlayerContext } from "./usePlayerContext";
 
 const useAudioPlayer = () => {
   const context = usePlayerContext();
   let player = context.ref;
 
   // save play audio function
-  const playAudio = useCallback(() => {
+  const playAudio = () => {
     context.forceGlobalAudioState.toPlay();
 
     const isPlaying = !context.globalAudioState.isPlaying;
     if (isPlaying === true) setTimeout(() => player?.play(), 20);
-  }, [player]);
+  };
 
   // save pause audio function
-  const pauseAudio = useCallback(() => {
+  const pauseAudio = () => {
     context.forceGlobalAudioState.toPause();
 
     const isPlaying = !context.globalAudioState.isPlaying;
     if (isPlaying === false) setTimeout(() => player?.pause(), 20);
-  }, []);
+  };
 
   // method for direct slider time change...
   // ...this will immediately impact the current time...
   // ... and appearance of the progress bar background
-  const seekTimeframe = useCallback((e) => {
+  const seekTimeframe = (e) => {
     player.currentTime = (player?.duration * e.target.value) / 100;
-  }, []);
+  };
 
   // method to jump forward 0.5s of the current time...
   // ...will freeze when it reached audio duration
-  const forwardAudio = useCallback(() => {
+  const forwardAudio = () => {
     if (player?.currentTime < player?.duration) player.currentTime += 0.5;
-  }, [player?.currentTime]);
+  };
 
   // method to jump backwards 0.5s of the current time...
   // ...will freeze when it reached audio duration
-  const backwardAudio = useCallback(() => {
-    if (player?.currentTime >= 0) {
-      player.currentTime -= 0.5;
-    }
-  }, [player?.currentTime]);
+  const backwardAudio = () => {
+    if (player?.currentTime >= 0) player.currentTime -= 0.5;
+  };
 
   /* EXPORTED COMPONENTS */
 
