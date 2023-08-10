05eec34edde1bf75f50bb4b1383c47e172d2e52a
Kent Ntwari <designerjehovah@gmail.com>
2023-08-10 06:52:40 -0400

    updated component

--- frontend/src/components/AudioPlaylist/index.js
+++ frontend/src/components/AudioPlaylist/index.js
@@ -31,40 +31,42 @@ const AudioPlaylist = () => {
 
   return (
     <>
-      <AudioModal ref={audioModal_ref} />
+      <AudioModal ref={audioModal_ref} playlist={playlist.slug} />
 
       <section className="px-5 py-4">
         <Link
           href="/browse"
-          className="underline underline-offset-3 text-sm text-neutral-200 decoration-neutral-200">
+          className="underline underline-offset-3 text-sm text-neutral-200 dark:text-neutral-20 decoration-neutral-200">
           Back to Browse
         </Link>
-        <article className="mt-6 px-3 py-4 rounded-lg bg-la-300 flex flex-col gap-3">
+        <article className="mt-6 px-3 py-4 rounded-lg bg-la-300 dark:bg-black-300 flex flex-col gap-3">
           <span className="w-fit px-3 py-2 rounded-full bg-white-300 text-xs text-black-300">
             Playlist
           </span>
           <div>
             <h3
-              className="capitalize font-semibold text-black-300"
+              className="capitalize font-semibold text-black-300 dark:text-white-300"
               aria-label="playlist title">
               {playlist.name}
             </h3>
-            <p className="mt-2 text-xs text-black-300" aria-label="playlist description">
+            <p
+              className="mt-2 text-xs text-black-300 dark:text-white-300"
+              aria-label="playlist description">
               {playlist.description}
             </p>
           </div>
         </article>
 
-        <div className="mt-6 flex flex-col gap-4">
+        <section className="mt-6 flex flex-col gap-4">
           <div className="flex items-center justify-between">
             <span className="grow font-semibold text-base">All messages</span>
-            <button className="px-2 py-1 border border-neutral-200 rounded flex items-center gap-2">
-              <AdjustmentsHorizontalIcon className="w-4 text-neutral-200" />
-              <span className="text-sm text-neutral-200">Filter</span>
+            <button className="px-2 py-1 border border-neutral-200 dark:border-white-300 rounded flex items-center gap-2">
+              <AdjustmentsHorizontalIcon className="w-4 text-neutral-200 dark:text-white-300" />
+              <span className="text-sm text-neutral-200 dark:text-white-300">Filter</span>
             </button>
           </div>
 
-          <div>
+          <div className="border border-la-300 dark:border-dp-200">
             {playlist.audios.map((details, index) => (
               <div
                 key={uuidv4()}
@@ -81,12 +83,16 @@ const AudioPlaylist = () => {
                 }}>
                 <AudioMessage.Piece
                   data={details}
-                  trackBg={index % 2 ? "bg-la-50" : "bg-la-75"}
+                  trackBg={
+                    index % 2
+                      ? "bg-la-50 hover:bg-la-100 dark:bg-dp-300 dark:hover:bg-dp-500"
+                      : "bg-la-75 hover:bg-la-100 dark:bg-dp-400 dark:hover:bg-dp-500"
+                  }
                 />
               </div>
             ))}
           </div>
-        </div>
+        </section>
       </section>
     </>
   );
