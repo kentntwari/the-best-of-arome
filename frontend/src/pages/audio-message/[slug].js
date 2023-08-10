e95e921b9ab98243e0fe87377f976e4b2481e72a
Kent Ntwari <designerjehovah@gmail.com>
2023-08-10 06:59:39 -0400

    updated page

--- frontend/src/pages/audio-message/[slug].js
+++ frontend/src/pages/audio-message/[slug].js
@@ -1,23 +1,26 @@
-import { useEffect, useRef, useCallback } from 'react';
+import { useEffect, useRef, useCallback } from "react";
 
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import { useSWRAudioState } from '@/hooks/useSWRAudioState';
+import { useSWRAudioState } from "@/hooks/useSWRAudioState";
 
-import { ChevronRightIcon } from '@heroicons/react/24/solid';
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
-import AudioPlayer from '@/components/AudioPlayer';
-import AudioPlaylist from '@/components/AudioPlaylist';
+import AudioPlayer from "@/components/AudioPlayer";
+import AudioPlaylist from "@/components/AudioPlaylist";
+import NextInQueue from "@/components/AudioMessage/NextInQueue";
 
-import { truncateText } from '@/utils/truncateText';
+import { truncateText } from "@/utils/truncateText";
 
 const AudioMessagePage = ({ data }) => {
-  // retrieve the data of the fetched audio message...
-  // ...fro the nested response attributes in data)
-  const { title, description, slug: audioSlug, playlist, audio } = data[0].attributes;
+  // retrieve the current playing audio ID for use of...
+  // ...fetching the next audio to play
+  const id = data[0].id;
 
   // retrieve the playlist slug
+  const { title, description, slug: audioSlug, playlist, audio } = data[0].attributes;
+
   const {
     data: {
       attributes: { slug: playlistSlug },
@@ -60,8 +63,10 @@ const AudioMessagePage = ({ data }) => {
         <AudioPlaylist />
       ) : (
         <article>
-          <main className="px-5 py-4 bg-la-300 flex flex-col gap-[60px]" role="message">
-            <div className="flex items-center gap-2 text-black-300">
+          <main
+            className="px-5 py-4 bg-la-300 dark:bg-dp-200 flex flex-col gap-15"
+            role="message">
+            <div className="flex items-center gap-2 text-black-300 dark:text-white-300">
               <Link
                 href={`/audio-message/${audioSlug}/?playlist=${playlistSlug}`}
                 className="text-xs">
@@ -75,26 +80,30 @@ const AudioMessagePage = ({ data }) => {
               <span className="w-fit px-3 py-2 rounded-full bg-white-300 text-xs text-black-300">
                 Audio post
               </span>
-              <h3 className="font-bold text-black-300">{title}</h3>
+              <h3 className="font-bold text-black-300 dark:text-white-300">{title}</h3>
             </div>
           </main>
 
-          <div className="bg-la-100 p-5">
+          <div className="bg-la-100 dark:bg-black-300 p-5">
             <AudioPlayer />
           </div>
 
           <section className="p-5 flex flex-col gap-5" role="details and more">
             <div className="flex flex-col gap-3" role="description">
               <p className="font-semibold text-base">Description</p>
-              <p ref={description_ref} className="text-sm text-justify text-black-300">
+              <p
+                ref={description_ref}
+                className="text-sm text-justify text-black-300 dark:text-white-300">
                 {truncateText(description, 320, false)}
                 <span
                   onClick={revealFullDescription}
-                  className="font-bold text-black-300">
+                  className="font-bold text-black-300 dark:text-white-300">
                   ...Read more
                 </span>
               </p>
             </div>
+
+            <NextInQueue id={parseInt(id) + 1} playlist={playlistSlug} />
           </section>
         </article>
       )}
@@ -103,13 +112,9 @@ const AudioMessagePage = ({ data }) => {
 };
 
 export async function getStaticPaths() {
-  // const res = await fetch('http://localhost:1337/api/audio-messages');
+  const res = await fetch("http://localhost:1337/api/audio-messages");
 
-  // for testing purposes
-  const test_res = await fetch(
-    'https://mocki.io/v1/82e531c2-4786-4c6c-b557-51d8e85e0823'
-  );
-  const { data } = await test_res.json();
+  const { data } = await res.json();
 
   // generate the paths
   const paths = data.map(({ attributes }) => ({
@@ -124,18 +129,13 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps(ctx) {
+export async function getStaticProps({ params }) {
   // get matching data with the slug of the fecthed audio message
-  // const res = await fetch(
-  //   `http://localhost:1337/api/audio-messages?filters[slug][$eq]=${params.slug}&populate[playlist][fields][0]=slug&populate[audio][fields][0]=alternativeText&populate[audio][fields][1]=url`
-  // );
-
-  // for testing purposes
-  const test_res = await fetch(
-    'https://mocki.io/v1/6cc51a1f-f0e8-4df1-ab0e-e02c67b8381b'
+  const res = await fetch(
+    `http://localhost:1337/api/audio-messages?filters[slug][$eq]=${params.slug}&populate[playlist][fields][0]=slug&populate[audio][fields][0]=url`
   );
 
-  const { data } = await test_res.json();
+  const { data } = await res.json();
 
   return {
     props: {
