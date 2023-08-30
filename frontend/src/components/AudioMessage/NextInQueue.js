61cac0461a483c55b3d79caa6ccdb29e98ccd72c
Kent Ntwari <designerjehovah@gmail.com>
2023-08-30 06:48:38 -0400

    updted components

--- frontend/src/components/AudioMessage/NextInQueue.js
+++ frontend/src/components/AudioMessage/NextInQueue.js
@@ -1,24 +1,33 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-import { useExtractFields } from "@/hooks/useExtractFields";
+import { useNormalizeQuery } from "@/hooks/useNormalizeQuery";
 
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 const NextInQueue = ({ id, playlist }) => {
-  const { nextInqueue } = useExtractFields("next in queue", { id, playlist });
+  const router = useRouter();
 
-  if (!nextInqueue) return;
+  const { nextInQueue } = useNormalizeQuery("get next message", {
+    id,
+    playlist: !router.query.q ? "" : playlist,
+  });
+
+  if (!nextInQueue || nextInQueue === {}) return;
 
   return (
     <>
-      <Link href={`/audio-message/${encodeURIComponent(nextInqueue.slug)}`}>
+      <Link
+        href={`/audio-message/${encodeURIComponent(nextInQueue.slug)}${
+          router.query.q ? "?q=" + router.query.q : ""
+        }`}>
         <div
-          className="px-2 py-3 rounded bg-ls-300 flex items-center justify-between"
+          className="px-2 py-3 rounded bg-ls-300 dark:bg-dp-200 flex items-center justify-between"
           role="next message in queue">
           <div className="max-w-[59.7%] flex flex-col gap-2">
             <span className="text-xs text-neutral-40">Next up</span>
             <span className="font-bold text-sm text-white-300">
-              {nextInqueue.title ?? "loading..."}
+              {nextInQueue.title ?? "loading..."}
             </span>
           </div>
 
--- frontend/src/components/AudioMessage/index.js
+++ frontend/src/components/AudioMessage/index.js
@@ -1,18 +1,4 @@
-import Piece from './Piece';
-import Snippets from './Snippets';
-import NextInQueue from './NextInQueue';
+import Snippets from "./Snippets";
+import NextInQueue from "./NextInQueue";
 
-const AudioMessage = {
-  // exports a Snippet of the message with just...
-  // a play button and a duration of the audio...
-  //...as well as a link to the full audio message post
-  Snippets,
-  // exports next audio message in playlist queue...
-  // ...with just the title and link to the data...
-  NextInQueue,
-  // exports a piece of an a playlist audio message...
-  // ...with just the title and the total duration
-  Piece,
-};
-
-export default AudioMessage;
+export { Snippets, NextInQueue };
