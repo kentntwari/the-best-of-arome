b2ba26670b37ac862747bc4c8fac38d518f12b43
Kent Ntwari <designerjehovah@gmail.com>
2023-08-30 06:49:38 -0400

    updted component

--- frontend/src/components/AudioPlayer/Duration.js
+++ frontend/src/components/AudioPlayer/Duration.js
@@ -4,30 +4,27 @@ import { usePlayerContext } from "@/hooks/usePlayerContext";
 
 import { convertToMinutesSeconds } from "../../utils/convertToMinutesSeconds";
 
-const Duration = ({
-  forceValue = null,
-  fontSize = null,
-  fontWeight = null,
-  color = null,
-}) => {
+const Duration = ({ value = null, fontSize = null, fontWeight = null, color = null }) => {
   const duration_ref = useRef();
 
   const context = usePlayerContext();
 
-  if (forceValue)
+  // provided value in props
+  if (value)
     return (
       <span
-        className={`${fontSize ?? "text-xs"} ${fontWeight && fontWeight} ${
+        className={`${fontSize ?? "text-xs lg:text-sm"} ${fontWeight && fontWeight} ${
           color ?? color
         }`}>
-        {forceValue}
+        {value}
       </span>
     );
 
+  // by default will display currently playing audio in context
   return (
     <span
       ref={duration_ref}
-      className={`${fontSize ?? "text-xs"} ${fontWeight && fontWeight} ${
+      className={`${fontSize ?? "text-xs lg:text-sm"} ${fontWeight && fontWeight} ${
         color ?? color
       }`}>
       {convertToMinutesSeconds(context.globalAudioState.duration) ?? "--:--"}
