b8ea10fef15642b9592de1f0f9dfa6121a7caf96
Kent Ntwari <designerjehovah@gmail.com>
2023-08-30 06:58:10 -0400

    updated layout tree

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import AudioBanner from "./AudioBanner";
 import SwitchTheme from "./SwitchTheme";
+import AudioPlaylist from "./AudioPlaylist";
 
 const Layout = ({ children }) => {
   const router = useRouter();
@@ -12,11 +13,20 @@ const Layout = ({ children }) => {
     <>
       <Navbar />
 
+      {router.pathname === "/" && <AudioBanner />}
       {router.pathname === "/browse" && !router.query.playlist && <AudioBanner />}
 
       <SwitchTheme />
-      
-      <div className="grow">{children}</div>
+
+      <div className="grow grid grid-cols-3">
+        {router.query.playlist && (
+          <>
+            <AudioPlaylist />
+            <div className="hidden lg:block lg:absolute lg:top-0 lg:w-full lg:h-full bg-black-300  opacity-20 z-10"></div>
+          </>
+        )}
+        <div className="row-span-full col-span-full">{children}</div>
+      </div>
 
       <Footer />
     </>
