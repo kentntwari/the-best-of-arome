313c70b7a5eb599cede915bbb74558c45790ccd5
Kent Ntwari <designerjehovah@gmail.com>
2023-09-24 17:44:21 -0400

    refactor: centralize components

--- frontend/src/components/AudioPlayer/index.js
+++ frontend/src/components/AudioPlayer/index.js
@@ -1,6 +1,5 @@
-import Test from "./Test";
 import Board from "./Board";
 import Wrapper from "./Wrapper";
 import TogglePlay from "./TogglePlay";
 
-export { Test, Board, Wrapper, TogglePlay };
+export { Board, Wrapper, TogglePlay };
