07adfbfc70902395fd25307346b5e54c08e54c52
Kent Ntwari <designerjehovah@gmail.com>
2023-09-24 17:42:42 -0400

    refactor: centralize components

--- frontend/src/components/AudioPlayer/index.js
+++ frontend/src/components/AudioPlayer/index.js
@@ -1,61 +1,6 @@
-import { useEffect } from "react";
+import Test from "./Test";
+import Board from "./Board";
+import Wrapper from "./Wrapper";
+import TogglePlay from "./TogglePlay";
 
-import { useRouter } from "next/router";
-
-import { useAudioPlayer } from "@/hooks/useAudioPlayer";
-
-import DefaultControls from "./DefaultControls";
-import DefaultFileReadings from "./DefaultFileReadings";
-
-const AudioPlayer = ({
-  enableDefaultFileReadings = true,
-  enableDefaultControls = true,
-  enableCustomization = false,
-  children,
-}) => {
-  const router = useRouter();
-
-  const { methods } = useAudioPlayer();
-
-  useEffect(() => {
-    if (router.pathname === "/audio-message/[slug]" && !router.query.playlist)
-      methods.pauseAudio();
-  }, [router.pathname]);
-
-  if (
-    enableCustomization === true &&
-    enableDefaultControls === false &&
-    enableDefaultFileReadings === false
-  ) {
-    return <>{children}</>;
-  }
-
-  if (enableCustomization === true && enableDefaultControls === false) {
-    return (
-      <>
-        <DefaultFileReadings />
-        {children}
-      </>
-    );
-  }
-
-  if (enableCustomization === true && enableDefaultFileReadings === false) {
-    return (
-      <>
-        {children}
-        <DefaultControls />
-      </>
-    );
-  }
-
-  return (
-    <>
-      <div className="flex flex-col gap-5">
-        <DefaultFileReadings />
-        <DefaultControls />
-      </div>
-    </>
-  );
-};
-
-export default AudioPlayer;
+export { Test, Board, Wrapper, TogglePlay };
