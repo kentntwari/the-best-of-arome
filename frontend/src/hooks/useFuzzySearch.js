69e011682a0ce4e26efd5147dcc64faa982bd92a
Kent Ntwari <designerjehovah@gmail.com>
2023-09-24 17:58:24 -0400

    feat:init search feature api based

---
+++ frontend/src/hooks/useFuzzySearch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useMemo } from "react";
+
+import useSWR from "swr";
+
+import debounce from "lodash.debounce";
+
+const useFuzzySearch = () => {
+  const [query, setQuery] = useState(null);
+
+  const { data } = useSWR(query ? `http://localhost:1337/api/search?q=${query}` : null);
+
+  const setDebouncedQuery = useMemo(
+    () => debounce((element) => setQuery(element?.target?.value ?? element), 500),
+    []
+  );
+
+  // Stop the invocation of the debounced function
+  // after unmounting
+  useEffect(() => {
+    return () => {
+      setDebouncedQuery.cancel();
+    };
+  }, []);
+
+  return [data, setDebouncedQuery];
+};
+
+export { useFuzzySearch };
