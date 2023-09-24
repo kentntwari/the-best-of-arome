6e4d2803db1296d77f34dc02db0ad532ea6df5c0
Kent Ntwari <designerjehovah@gmail.com>
2023-09-24 18:06:45 -0400

    styles:update styles

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,10 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
+
+import { poppins } from "@/styles/fonts";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className={poppins.variable}>
       <Head />
-      <body className="lg:relative bg-lp-300 dark:bg-dp-300 text-ls-300 dark:text-white-300 text-sm lg:text-base">
+      <body className="lg:relative xl:overflow-hidden bg-lp-300 dark:bg-dp-300 text-ls-300 dark:text-white-300 text-sm lg:text-base">
         <Main />
         <NextScript />
       </body>
