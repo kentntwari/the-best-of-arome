7322279b3311de56879a2f036fb242ec8ba599c0
Kent Ntwari <designerjehovah@gmail.com>
2023-09-24 17:48:23 -0400

    style: add tailwind classes for responsivity

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer className="px-5 lg:px-10 mb-4 text-2xs lg:text-xs text-right">The Best of Apostle Arome Osayi. 2023</footer>;
+  return <footer className="mt-6 px-5 lg:px-10 mb-4 text-2xs lg:text-xs text-right">The Best of Apostle Arome Osayi. 2023</footer>;
 };
 
 export default Footer;
