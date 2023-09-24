59043aa1122ba5c6978aef1f4a2483bf627ad770
Kent Ntwari <designerjehovah@gmail.com>
2023-09-24 17:54:15 -0400

    refactor:removed per page router logic playlist modal

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,30 +1,33 @@
+import { useEffect } from "react";
+
 import { useRouter } from "next/router";
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import AudioBanner from "./AudioBanner";
 import SwitchTheme from "./SwitchTheme";
-import AudioPlaylist from "./AudioPlaylist";
 
 const Layout = ({ children }) => {
   const router = useRouter();
+
+  useEffect(() => {
+    let mounted = true;
+
+    if (mounted && document.body.hasAttribute("style")) {
+      document.body.removeAttribute("style");
+    }
+
+    return () => {
+      mounted = false;
+    };
+  }, [router]);
 
   return (
     <>
       <Navbar />
 
-      {router.pathname === "/" && <AudioBanner />}
-      {router.pathname === "/browse" && !router.query.playlist && <AudioBanner />}
-
       <SwitchTheme />
 
       <div className="grow grid grid-cols-3">
-        {router.query.playlist && (
-          <>
-            <AudioPlaylist />
-            <div className="hidden lg:block lg:absolute lg:top-0 lg:w-full lg:h-full bg-black-300  opacity-20 z-10"></div>
-          </>
-        )}
         <div className="row-span-full col-span-full">{children}</div>
       </div>
 
