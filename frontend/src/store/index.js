34fd05a0f6b23ec1f8b1c106a78fbfba1c313e78
Kent Ntwari <designerjehovah@gmail.com>
2023-10-12 22:55:43 -0400

    frat:update store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,43 @@
-import { proxy } from "valtio";
+import { proxy, ref } from "valtio";
 
 export const store = proxy({
-  currentAudio: null,
-  isPlaying: false,
+  currentAudio: {
+    global: null,
+    scoped: null,
+  },
+  isPlaying: {
+    global: false,
+    scoped: false,
+  },
 });
+
+export const actions = {
+  setGlobal: (audioSrc) => {
+    actions.pauseAll();
+    store.currentAudio.global = audioSrc;
+  },
+  setScoped: (audioSrc) => {
+    actions.pauseAll();
+    store.currentAudio.scoped = audioSrc;
+  },
+  playGlobal: (audioSrc) => {
+    actions.pauseAll();
+    store.currentAudio.global = audioSrc;
+    store.isPlaying.global = true;
+  },
+  playScoped: (audioSrc) => {
+    actions.pauseAll();
+    store.currentAudio.scoped = audioSrc;
+    store.isPlaying.scoped = true;
+  },
+  pauseGlobal: () => {
+    store.isPlaying.global = false;
+  },
+  pauseScoped: () => {
+    store.isPlaying.scoped = false;
+  },
+  pauseAll: () => {
+    if (store.isPlaying.scoped) actions.pauseScoped();
+    if (store.isPlaying.global) actions.pauseGlobal();
+  },
+};
